1de0c7737a7e6b0b64886764c256b66d48753cc0
Hari Krishnan <vijayharish25@gmail.com>
2025-11-26 21:08:05 +0530

    - Updated the next js latest syntax

--- Blogify/app/api/posts/[id]/route.ts
+++ Blogify/app/api/posts/[id]/route.ts
@@ -1,14 +1,12 @@
 import { posts } from '@/lib/posts';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ): Promise<NextResponse> {
   try {
-    // Extract id from URL instead of params
-    const url = new URL(request.url);
-    const id = url.pathname.split("/").pop() || "";
+    const { id } = await params;
 
     const post = posts.find((post) => post.id === id);
 
